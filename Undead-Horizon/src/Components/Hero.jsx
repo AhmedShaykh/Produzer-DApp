4ea767da306dda674f10b3946a3afc31dcb3901b
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-03-04 20:54:35 +0500

    Final Commit

--- Undead-Horizon/src/Components/Hero.jsx
+++ Undead-Horizon/src/Components/Hero.jsx
@@ -1,14 +1,10 @@
 const Hero = () => {
     return (
         <div
-            className="w-full bg-center bg-cover bg-no-repeat"
+            className="bgImg"
             name="home"
-            style={{
-                backgroundImage:
-                    "url(/assets/dead.webp)"
-            }}
         >
-            <div className="flex items-center justify-center bg-black bg-opacity-50 w-full h-full py-72 px-5 sm:px-20">
+            <div className="flex items-center justify-center bg-black bg-opacity-50 w-full h-full py-48 md:py-80 px-5 sm:px-20">
                 <div className="wrapper">
                 </div>
             </div>
